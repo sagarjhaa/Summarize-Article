f3eba8aa800f791a7dc106f84109843d798570b5
Sagar Jha <sagar.jhaa@gmail.com>
2017-09-18 12:00:31 -0400

    Add project files.

---
+++ Summarize_WashingtonPost/routes.py
@@ -0,0 +1,65 @@
+"""
+Routes and views for the bottle application.
+"""
+
+from bottle import route, view , get,post,request
+from datetime import datetime
+import urllib.request
+from bs4 import BeautifulSoup
+
+@route('/')
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year,
+        OriginalText = 'Orignal',
+        Summary = 'String'
+    )
+
+@route('/contact')
+@view('contact')
+def contact():
+    """Renders the contact page."""
+    return dict(
+        title='Contact',
+        message='Your contact page.',
+        year=datetime.now().year
+    )
+
+@route('/about')
+@view('about')
+def about():
+    """Renders the about page."""
+    return dict(
+        title='About',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
+
+
+@post('/login')
+@view('index')
+def do_post():
+    
+    articleUrl = request.forms.get('articleURL')
+    no_of_lines = request.forms.get('noOfLines')
+
+    #Get the original text
+    #Get the summarized text
+
+    text = getTextWaPo(articleUrl);
+
+    return dict(year = datetime.now().year,
+                OriginalText=text,
+                Summary="Summary -Sagar Jha")
+   
+
+
+
+def getTextWaPo(url):
+    page = urllib.request.urlopen(url).read().decode('utf8')
+    soup = BeautifulSoup(page,'html.parser')
+    text = ' '.join(map(lambda p: p.text,soup.find_all('article')))
+    return text.encode('ascii',errors='replace')#.replace('?'," ")
---
+++ Summarize_WashingtonPost/views/index.tpl
@@ -0,0 +1,43 @@
+% rebase('layout.tpl', title='Home Page', year=year)
+
+
+<br />
+<br />
+<form action="/login" method="post" class="form-inline">
+    <div class="row">
+        <div class="form-group col-md-7" >
+            <input name="articleURL" class="form-control " style="max-width:100%" type="url" required placeholder="Washington post article url"/>
+        </div>
+
+        <div class="form-group col-md-3">
+            <input name="noOfLines" class="form-control" type="number" placeholder="Lines in summary"/>
+        </div>
+
+        <div class="form-group col-md-2">
+            <input id="submit" type="Submit" class="btn btn-default" value="Summarize" />
+        </div>
+
+    </div>
+</form>
+
+<br />
+<br />
+<br />
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <p class="text-justify ">{{OriginalText}}</p>
+        </div>
+
+        <div class="col-md-6">
+            {{Summary}}
+        </div>
+    </div>
+</div>
+<!--<script>
+    $("#submit").submit(function (d) {
+        e.preventDefault();
+        alert("sagar jha");
+    });
+</script>-->
+
